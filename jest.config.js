8bad74d1622235788ca9445e11f0a0dd69cfcbac
bizbaeja <bjgbjg112@gmail.com>
2023-03-09 18:18:40 +0900

    heroku

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+console.log(process.version);
+new TextEncoder();
+module.exports = {
+  testEnvironment: "node",
+};
+import { TextEncoder, TextDecoder } from "util";
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+module.exports = {
+  transform: {
+    "^.+\\.(js|jms)$": "js-jest",
+  },
+};
